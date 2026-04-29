ab1409dc8545111822cb6bc0bb5a683c4d26f3cf
LStoria <ducminhphung.23@gmail.com>
2026-04-29 23:32:04 +0700

    Thêm 1 số class
    Thêm các class kế thừa item, thêm thuộc tính email cho User

---
+++ server/src/main/java/Art.java
@@ -0,0 +1,10 @@
+public class Art extends Item {
+
+    private String artist;
+
+    public Art(String name, double price, String artist) {
+        super(name, price);
+        this.artist = artist;
+    }
+
+}
