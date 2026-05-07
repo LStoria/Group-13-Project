f1f18701a576a30d0a625553952d22a4f4af8a4c
nguyenthox4dd01012007-dotcom <nguyenthox4dd01012007@gmail.com>
2026-05-07 18:45:33 +0700

    update demo chạy thử

---
+++ client/src/main/java/view/MainApp.java
@@ -0,0 +1,8 @@
+package view;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("🚀 Client đang khởi động...");
+        // Sau này Người 4 sẽ viết code JavaFX ở đây
+    }
+}
