0b7ed1d960193fe93afa83b1daa0db61fe056041
LStoria <ducminhphung.23@gmail.com>
2026-05-08 13:56:33 +0700

    Thêm setter và auction repository

--- server/src/main/java/model/Entity.java
+++ server/src/main/java/model/Entity.java
@@ -1,5 +1,15 @@
 package model;
 
 public abstract class Entity {
+
     protected Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    protected void setId(Long id) {
+        this.id = id;
+    }
+
 }
