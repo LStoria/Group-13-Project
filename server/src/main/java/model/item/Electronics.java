5c0ed1a699304de81e24858dc3ab2066112ef245
LStoria <ducminhphung.23@gmail.com>
2026-05-28 02:18:02 +0700

    Thêm seller vào thuộc tính của item

--- server/src/main/java/model/item/Electronics.java
+++ server/src/main/java/model/item/Electronics.java
@@ -1,12 +1,17 @@
 package model.item;
 
+import model.user.User;
+
 public class Electronics extends Item {
 
     private String brand;
 
-    public Electronics(String name, double price, String brand) {
-        super(name, price);
+    public Electronics(String name, User seller, String description, double startPrice, double currentPrice, String brand) {
+        super(name, seller, description, startPrice, currentPrice);
         this.brand = brand;
     }
 
+    public String getBrand() {
+        return brand;
+    }
 }
